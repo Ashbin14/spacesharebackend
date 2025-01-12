882aff2b6784fd2b060cb92e5c659168928c8f6f
ashbin14 <ashbinthapachhetri@gmail.com>
2025-01-12 08:35:44 +0545

    resolve

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.use((error, req, res, next) => {
   res.status(500).json({ error: error.message });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
