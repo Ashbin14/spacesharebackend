35776cbf0081903811f1362e13c9f40e8328d24b
sumitpati7 <sumitpati7@gmail.com>
2025-01-14 22:26:58 +0545

    login fix

--- models/user.js
+++ models/user.js
@@ -1,49 +1,47 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name is required']
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, 'Must be at least 18 years old']
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ['male', 'female', 'other']
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    images: [{
+      type: String
+    }]
   },
-  lastName: {
-    type: String,
-    required: [true, 'Last name is required']
-  },
-  age: {
-    type: Number,
-    required: [true, 'Age is required'],
-    min: [18, 'Must be at least 18 years old']
-  },
-  gender: {
-    type: String,
-    required: [true, 'Gender is required'],
-    enum: ['male', 'female', 'other']
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, 'Phone number is required']
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: 8,
-    select: false
-  },
-  images: [{
-    type: String
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true, 
   }
-});
+);
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
